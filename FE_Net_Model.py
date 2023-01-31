06b0eaee6984b1d5af455f2e21e9565eb25bc4c2
OMEGA-RS <123748394+OMEGA-RS@users.noreply.github.com>
2023-01-31 11:04:30 +0800

    Add files via upload

---
+++ FE_Net_Model.py
@@ -0,0 +1,28 @@
+import os
+import torch
+import torch.nn as nn
+from Net_Model import SiameseNet
+
+MODEL_PATH = os.path.join('Data\Guangzhou', 'Pretrained_Model.pt')
+
+class FE_Net(nn.Module):
+
+    def __init__(self):
+        super(FE_Net, self).__init__()
+
+        Pretrained_Model = SiameseNet()
+        Pretrained_Model.load_state_dict(torch.load(MODEL_PATH))
+
+        self.features = Pretrained_Model
+
+    def forward(self, x):
+
+        x1, x2, x3, x4 = self.features.input_forward(x)
+
+        x1 = x1.view(x1.size()[0], -1)
+        x2 = x2.view(x2.size()[0], -1)
+        x3 = x3.view(x3.size()[0], -1)
+        x4 = x4.view(x4.size()[0], -1)
+
+        return x1, x2, x3, x4
+
